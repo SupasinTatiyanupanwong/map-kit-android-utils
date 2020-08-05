fb8084b4ef12928d3b0857e260d5344ccb7089da
Supasin Tatiyanupanwong <supasin@tatiyanupanwong.me>
2020-08-05 20:06:05 +0700

    Project init

---
+++ sample/src/main/java/me/tatiyanupanwong/supasin/android/samples/kits/maps/utils/MainActivity.java
@@ -0,0 +1,11 @@
+package me.tatiyanupanwong.supasin.android.samples.kits.maps.utils;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    public MainActivity() {
+        super(R.layout.activity_main);
+    }
+
+}

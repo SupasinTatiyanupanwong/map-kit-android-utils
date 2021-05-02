f110687efa4841f7b0fc2e65b353bfa9e84955db
Supasin Tatiyanupanwong <supasin@tatiyanupanwong.me>
2021-05-02 19:45:47 +0700

    Moved to the new namespace

---
+++ sample/src/main/java/dev/supasintatiyanupanwong/samples/android/kits/maps/utils/ClusteringViewModel.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2020 Supasin Tatiyanupanwong
+ * Copyright 2019 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.supasintatiyanupanwong.samples.android.kits.maps.utils;
+
+import android.content.res.Resources;
+
+import androidx.lifecycle.ViewModel;
+
+import org.json.JSONException;
+
+import java.io.InputStream;
+import java.util.List;
+
+import dev.supasintatiyanupanwong.libraries.android.kits.maps.model.LatLng;
+import dev.supasintatiyanupanwong.libraries.android.kits.maps.utils.clustering.algo.NonHierarchicalViewBasedAlgorithm;
+import dev.supasintatiyanupanwong.samples.android.kits.maps.utils.model.MyItem;
+
+public class ClusteringViewModel extends ViewModel {
+
+    private NonHierarchicalViewBasedAlgorithm<MyItem> mAlgorithm =
+            new NonHierarchicalViewBasedAlgorithm<>(0, 0);
+
+    NonHierarchicalViewBasedAlgorithm<MyItem> getAlgorithm() {
+        return mAlgorithm;
+    }
+
+    void readItems(Resources resources) throws JSONException {
+        InputStream inputStream = resources.openRawResource(R.raw.radar_search);
+        List<MyItem> items = new MyItemReader().read(inputStream);
+        mAlgorithm.lock();
+        try {
+            for (int i = 0; i < 100; i++) {
+                double offset = i / 60d;
+                for (MyItem item : items) {
+                    LatLng position = item.getPosition();
+                    double lat = position.getLatitude() + offset;
+                    double lng = position.getLongitude() + offset;
+                    MyItem offsetItem = new MyItem(lat, lng);
+                    mAlgorithm.addItem(offsetItem);
+                }
+            }
+        } finally {
+            mAlgorithm.unlock();
+        }
+    }
+}
---
+++ sample/src/main/java/dev/supasintatiyanupanwong/samples/android/kits/maps/utils/TileProviderAndProjectionDemo.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2020 Supasin Tatiyanupanwong
+ * Copyright 2013 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.supasintatiyanupanwong.samples.android.kits.maps.utils;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import dev.supasintatiyanupanwong.libraries.android.kits.maps.MapKit;
+import dev.supasintatiyanupanwong.libraries.android.kits.maps.model.LatLng;
+import dev.supasintatiyanupanwong.libraries.android.kits.maps.model.Tile;
+import dev.supasintatiyanupanwong.libraries.android.kits.maps.model.TileProvider;
+import dev.supasintatiyanupanwong.libraries.android.kits.maps.utils.geometry.Point;
+import dev.supasintatiyanupanwong.libraries.android.kits.maps.utils.projection.SphericalMercatorProjection;
+
+public class TileProviderAndProjectionDemo extends BaseDemoActivity {
+
+    @Override
+    protected void startDemo(boolean isRestore) {
+        PointTileOverlay pto = new PointTileOverlay();
+        pto.addPoint(MapKit.newLatLng(0, 0));
+        pto.addPoint(MapKit.newLatLng(21, -10));
+        getMap().addTileOverlay(MapKit.newTileOverlayOptions().tileProvider(pto));
+    }
+
+    private class PointTileOverlay implements TileProvider {
+        private List<Point> mPoints = new ArrayList<>();
+        private int mTileSize = 256;
+        private SphericalMercatorProjection mProjection = new SphericalMercatorProjection(mTileSize);
+        private int mScale = 2;
+        private int mDimension = mScale * mTileSize;
+
+        @Override
+        public Tile getTile(int x, int y, int zoom) {
+            Matrix matrix = new Matrix();
+            float scale = (float) Math.pow(2, zoom) * mScale;
+            matrix.postScale(scale, scale);
+            matrix.postTranslate(-x * mDimension, -y * mDimension);
+
+            Bitmap bitmap = Bitmap.createBitmap(mDimension, mDimension, Bitmap.Config.ARGB_8888);
+            Canvas c = new Canvas(bitmap);
+            c.setMatrix(matrix);
+
+            for (Point p : mPoints) {
+                c.drawCircle((float) p.x, (float) p.y, 1, new Paint());
+            }
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+            return MapKit.newTile(mDimension, mDimension, baos.toByteArray());
+        }
+
+        public void addPoint(LatLng latLng) {
+            mPoints.add(mProjection.toPoint(latLng));
+        }
+    }
+}
